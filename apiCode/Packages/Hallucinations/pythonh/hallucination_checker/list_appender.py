c8da55abc40f3ad68cd6ba710c7d0a68be272351
Susannah L <susannahlyon18@gmail.com>
2025-11-19 10:57:13 -0500

    fixed list_appender.py bug

--- apiCode/Packages/Hallucinations/pythonh/hallucination_checker/list_appender.py
+++ apiCode/Packages/Hallucinations/pythonh/hallucination_checker/list_appender.py
@@ -1,13 +1,5 @@
-'''
-want to be able to read in the current file and compare each package on the current hallucination list to 
-all of the packages in the master hallucaination list.
-
-for each line in master read. if this package not on list, appeand to list.
-
-'''
-
 PRIMARY_LIST = "primarylist.txt"
-CURRENT_LIST = "../DS3reasonerhalluncinations.txt" # DS3chathalluncinations.txt DS3reasonerhalluncinations.txt
+CURRENT_LIST = "../DS3chathalluncinations.txt" # DS3chathalluncinations.txt DS3reasonerhalluncinations.txt
 
 def main():
 
@@ -24,8 +16,9 @@ def main():
             primaryappend.write("\n____________\n"+ CURRENT_LIST+ "\n____________\n")
             for current_line in currentlist:
                 line_in_current = current_line.strip()
+                line_in_current = '"' + line_in_current + '"' +","
                 if line_in_current not in lines_in_primary:
-                    primaryappend.write("\n" + '"' + line_in_current + '"'+",")
+                    primaryappend.write("\n" + line_in_current)
                     "\"A word that needs quotation marks\""
                     lines_in_primary.add(line_in_current)
     except Exception as e:
